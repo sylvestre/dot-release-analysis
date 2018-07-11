6d179b75ee362768396df210cf45c3d10ec24da1
Sylvestre Ledru <sylvestre@debian.org>
2018-07-11 19:51:02 +0200

    appropriate exit code

--- missing-bug.py
+++ missing-bug.py
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
 from analysis import analyzeFiles
 
 if __name__ == "__main__":
     bugs, nbUnset, total = analyzeFiles(['Firefox', 'Firefox for Android'])
+    if nbUnset > 0:
+        sys.exit(43)
