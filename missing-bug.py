1de9de433d2904d27e5b2806896887c977573edc
Sylvestre Ledru <sylvestre@debian.org>
2018-07-11 19:27:51 +0200

    add a script to highlight missing bug in the list

---
+++ missing-bug.py
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from analysis import analyzeFiles
+
+if __name__ == "__main__":
+    bugs, nbUnset, total = analyzeFiles(['Firefox', 'Firefox for Android'])
